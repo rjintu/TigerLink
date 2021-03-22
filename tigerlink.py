f2d6e503bf26861ca24a83e860b368d84794e8a2
Devon Ulrich <dpulrich@gmail.com>
2021-03-22 15:17:01 -0400

    Testing database coding

--- tigerlink.py
+++ tigerlink.py
@@ -1,3 +1,4 @@
+from os import environ
 from flask import Flask, request, make_response, redirect
 from flask import render_template
 
@@ -5,6 +6,7 @@ app = Flask(__name__, template_folder='.')
 
 @app.route('/', methods=['GET'])
 def index():
-    html = "hello, world!"
+    html = "hello, world! <br>"
+    html += environ.get('DATABASE_URL')
     response = make_response(html)
     return response
