d7bd2396f744ea86ff922a4d29731b862bc33426
Devon Ulrich <dpulrich@gmail.com>
2021-03-22 16:01:08 -0400

    Added tigerlink.py; should've been part of previous commit

--- tigerlink.py
+++ tigerlink.py
@@ -1,12 +1,21 @@
-from os import environ
 from flask import Flask, request, make_response, redirect
 from flask import render_template
+
+from database import Database
 
 app = Flask(__name__, template_folder='.')
 
 @app.route('/', methods=['GET'])
 def index():
-    html = "hello, world! <br>"
-    html += environ.get('DATABASE_URL')
+    try:
+        db = Database()
+        db.connect()
+        db.create_students()
+        db.disconnect()
+        html = "database ran successfully!"
+    except Exception as e:
+        html = "error occurred: " + str(e)
+        print(e)
+
     response = make_response(html)
     return response
