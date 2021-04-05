871df677904dd514902221fb22f063b8430b244b
Devon Ulrich <dpulrich@gmail.com>
2021-04-04 21:59:53 -0400

    Created separate class for handling auth stuff
    This luckily cleans up tigerlink.py a lot.

--- server/loginutil.py
+++ server/loginutil.py
@@ -1,11 +1,80 @@
+from os import environ
+from sys import stderr
+from oauthlib.oauth2 import WebApplicationClient
 import requests
-from . import keys # file *not* in git containing our client id + key
+import json
 
-GOOGLE_CLIENT_ID = keys.CL_ID
-GOOGLE_CLIENT_SECRET = keys.CL_SECRET
+
 GOOGLE_DISCOVERY_URL = (
         "https://accounts.google.com/.well-known/openid-configuration"
     )
 
 def get_google_provider_cfg():
     return requests.get(GOOGLE_DISCOVERY_URL).json()
+
+class GoogleLogin:
+
+    # set up keys & the oauth client
+    def __init__(self):
+        self._CLIENT_ID = environ.get('GOOGLE_CLIENT_ID')
+        self._CLIENT_SECRET = environ.get('GOOGLE_CLIENT_SECRET')
+        if self._CLIENT_ID is None:
+            print('Could not load keys from environment variables')
+            print('Loading keys through keys.py...')
+            try:
+                from . import keys
+                self._CLIENT_ID = keys.GOOGLE_CLIENT_ID
+                self._CLIENT_SECRET = keys.GOOGLE_CLIENT_SECRET
+            except Exception as e:
+                print("Error: could not load secret keys!", file=stderr)
+                print("Do you have keys.py in the 'server' folder?", file=stderr)
+                print("More info: " + str(e))
+                exit(1)
+
+        self._oauth_cl = WebApplicationClient(self._CLIENT_ID)
+
+    def get_login_redirect(self, request):
+        # find out which URL to redirect to
+        google_provider_cfg = get_google_provider_cfg()
+        auth_endpoint = google_provider_cfg["authorization_endpoint"]
+
+        request_uri = self._oauth_cl.prepare_request_uri(
+            auth_endpoint,
+            redirect_uri = request.base_url + "/auth",
+            scope = ["openid", "email", "profile"],
+        )
+        return request_uri
+
+    def authorize(self, request):
+        # refer to https://realpython.com/flask-google-login/ for more info
+        google_provider_cfg = get_google_provider_cfg()
+        token_endpoint = google_provider_cfg["token_endpoint"]
+
+        # get the code that google sent back to us
+        code = request.args.get('code')
+
+        # prep request to get tokens
+        token_url, headers, body = self._oauth_cl.prepare_token_request(
+            token_endpoint,
+            authorization_response = request.url,
+            redirect_url = request.base_url,
+            code = code
+        )
+        token_response = requests.post(
+            token_url,
+            headers = headers,
+            data = body,
+            auth = (self._CLIENT_ID, self._CLIENT_SECRET),
+        )
+
+        # parse the tokens
+        self._oauth_cl.parse_request_body_response(json.dumps(token_response.json()))
+
+        # now get user info from google
+        userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
+        uri, headers, body = self._oauth_cl.add_token(userinfo_endpoint)
+        userinfo_response = requests.get(uri, headers=headers, data=body)
+
+        # now get all relevant user data
+        info = userinfo_response.json()
+        return (info['sub'], info['email'], info['name'])
--- server/tigerlink.py
+++ server/tigerlink.py
@@ -12,7 +12,7 @@ from .cookiemonster import CookieMonster
 from . import loginutil
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
-oauth_cl= WebApplicationClient(loginutil.GOOGLE_CLIENT_ID)
+login_manager = loginutil.GoogleLogin()
 
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
@@ -24,65 +24,16 @@ def index():
 # Note: when testing locally, must use port 8888 for Google SSO
 @app.route('/login', methods=['GET'])
 def login():
-    # Find out what URL to hit for Google login
-    google_provider_cfg = loginutil.get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-    request_uri = oauth_cl.prepare_request_uri(
-        authorization_endpoint,
-        redirect_uri= request.base_url + "/auth",
-        scope=["openid", "email", "profile"],
-    )
+    # redirect the user to Google's login page
+    request_uri = login_manager.get_login_redirect(request)
     return redirect(request_uri)
-    
 
 # for checking if user exists already, setting a session cookie,
 # and redirecting to the next page
 @app.route('/login/auth', methods=['GET'])
 def login_auth():
-    code = request.args.get('code')
-
-    # Find out what URL to hit to get tokens that allow you to ask for
-    # things on behalf of a user
-    google_provider_cfg = loginutil.get_google_provider_cfg()
-    token_endpoint = google_provider_cfg["token_endpoint"]
-
-    # Prepare and send a request to get tokens! Yay tokens!
-    token_url, headers, body = oauth_cl.prepare_token_request(
-        token_endpoint,
-        authorization_response=request.url,
-        redirect_url=request.base_url,
-        code=code
-    )
-    token_response = requests.post(
-        token_url,
-        headers=headers,
-        data=body,
-        auth=(loginutil.GOOGLE_CLIENT_ID, loginutil.GOOGLE_CLIENT_SECRET),
-    )
-
-    # Parse the tokens!
-    oauth_cl.parse_request_body_response(json.dumps(token_response.json()))
-
-    # Now that you have tokens (yay) let's find and hit the URL
-    # from Google that gives you the user's profile information,
-    # including their Google profile image and email
-    userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
-    uri, headers, body = oauth_cl.add_token(userinfo_endpoint)
-    userinfo_response = requests.get(uri, headers=headers, data=body)
-
-    # You want to make sure their email is verified.
-    # The user authenticated with Google, authorized your
-    # app, and now you've verified their email through Google!
-    if userinfo_response.json().get("email_verified"):
-        unique_id = userinfo_response.json()["sub"]
-        users_email = userinfo_response.json()["email"]
-        picture = userinfo_response.json()["picture"]
-        users_name = userinfo_response.json()["given_name"]
-    else:
-        return "User email not available or not verified by Google.", 400
-
-    return make_response(userinfo_response.json())
+    profileid, email, fullname = login_manager.authorize(request)
+    return make_response(profileid + ": " + fullname)
 
 @app.route('/createstudent', methods=['POST'])
 def createstudent():
