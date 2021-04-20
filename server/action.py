3764dd8d09a521f34874eb9697f6c7a6dff62da2
Sahil Jain <sahil@princeton.edu>
2021-04-20 00:51:56 -0400

    action.py

---
+++ server/action.py
@@ -0,0 +1,36 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+# The mail addresses and password
+sender_address = 'princetontigerlink@gmail.com'
+sender_pass = 'tigerlink-cos333-2021'
+
+def emailUser(receiver_address, name, acct_type, class_year):
+    mail_content = '''Hello TigerLink User,
+
+Your account is now registered. Here's the information you provided us:
+Name:
+Email Address: 
+Type of Account: 
+Class Year: 
+
+Excited to have you,
+TigerLink Team
+'''
+
+
+    # Setup the MIME
+    message = MIMEMultipart()
+    message['From'] = sender_address
+    message['To'] = receiver_address
+    message['Subject'] = 'Congrats! You made a TigerLink Account.'   #The subject line
+    #The body and the attachments for the mail
+    message.attach(MIMEText(mail_content, 'plain'))
+    #Create SMTP session for sending the mail
+    session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
+    session.starttls() #enable security
+    session.login(sender_address, sender_pass) #login with mail_id and password
+    text = message.as_string()
+    session.sendmail(sender_address, receiver_address, text)
+    session.quit()
+    print('Mail Sent')
