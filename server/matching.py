2d6f1d3b4ceabd79e9541d3a6be3470f0d9a2955
Ayush Alag <aalag@princeton.edu>
2021-05-07 16:47:02 -0400

    fix

--- server/matching.py
+++ server/matching.py
@@ -95,11 +95,11 @@ class Matching(object):
         matches = []
         fm = []
         for match in self._curMatches:
+            if match[0] in self._students:
+                match[0]._numMatch -= 1
+            if match[1] in self._alumni:
+                match[1]._numMatch -= 1
             matches.append((match[0], match[1]))
-            # if match[0] in self._students:
-            #     match[0]._numMatch -= 1
-            # if match[1] in self._alumni:
-            #     match[1]._numMatch -= 1
             fm.append((match[0]._profileid, match[1]._profileid, match[2], match[3], match[4], match[5], dotProduct(match[0], match[1])))
 
         return matches, fm
